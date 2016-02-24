7275ef2cb7f73c8b97dc27259251acd684f17e01
Grant Redfearn <grantredfearn@gmail.com>
2016-02-24 17:26:18 -0600

    request sending. error on request to API

--- client/components/SuggestionItem.jsx
+++ client/components/SuggestionItem.jsx
@@ -1,17 +1,26 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var DeleteHelper = require('../requests/delete.js');
-var Votes = require('../models/votes.js');
+var PostHelper = require('../requests/post.js');
 
 var SuggestionItem = React.createClass({
   clickHandler: function() {
     var id = {};
     id.id_suggestion = this.props.suggestion.id;
+
     DeleteHelper.deleteSuggestion(id);
   },
 
   voteHandler: function() {
-    alert('You clicked the up arrow!');
+    var userObject = {};
+    userObject.suggestionID = this.props.suggestion.id;
+    userObject.userID = this.props.suggestion.id_user;
+
+    console.log('Props R Us', this.props)
+
+    PostHelper.upVoteSuggestion(userObject)
+
+    // this.props.upVoteSuggestion(this.props.suggestion.id)
   },
 
   render: function() {
