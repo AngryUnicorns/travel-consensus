15209164f2a083e32c6dd85e781e110a1f208931
Clay Branch <clay.branch@gmail.com>
2016-02-26 17:48:45 -0600

    added icon that updates when a user hasn't voted

--- client/components/TaskItem.jsx
+++ client/components/TaskItem.jsx
@@ -8,16 +8,20 @@ var TaskItem = React.createClass({
   },
 
   render: function() {
+    var circleClass = this.props.task.user_needs_to_vote ? "voted-icon fa fa-circle-thin" : "voted-icon fa fa-circle";
     if(this.props.task.consensus_choice){
       return (
         <div className="task-item" onClick={this.clickHandler}>
-          <p>{this.props.task.name + "-" + this.props.task.consensus_choice}</p>
+          <p>{this.props.task.name + " : " + this.props.task.consensus_choice}</p>
         </div>
         )
     } else {
       return (
         <div className="task-item" onClick={this.clickHandler}>
-          <p>{this.props.task.name}</p>
+          <p>
+          {this.props.task.name}
+            <i className={circleClass}></i>
+          </p>
         </div>
       )
     }
