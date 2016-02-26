29803adaf39dd389fb9fe826a1e79ebb8cf3f15f
Clay Branch <clay.branch@gmail.com>
2016-02-26 16:24:47 -0600

    reworked fetch process to work with  multiple tasks

--- client/requests/get.js
+++ client/requests/get.js
@@ -1,27 +1,42 @@
 require('./request-helpers.js'); // Imports headers
 require('whatwg-fetch');      // imports 'fetch' function
 var User = require('../models/users');
+var _ = require('underscore');
+var cachedTasks = {};
+var Task = require('../models/tasks');
 
 exports.setViewDataUpdateInterval = function(taskList, taskArea, interval) {
   setInterval(function() {
+    
     fetchTasks(window.globalStateTripId)
-      .then(function(tasks) {
-        taskList.setState( {tasksInList: tasks} );
+    .then(function(tasks){
+      var allFetches = [];
+      tasks.forEach(function(task){
+        if(window.globalStateTaskId === null) window.globalStateTaskId = task.id;
+        var fetch = fetchSuggestions(task.id).then(function(suggestions){
+          cachedTasks[task.id] = task;
+          cachedTasks[task.id]['suggestions'] = suggestions;
+        });
+        allFetches.push(fetch); 
       })
+      return Promise.all(allFetches);
+    }).then(function(){
+      var suggestions = cachedTasks[window.globalStateTaskId]['suggestions'];
+      _.each(cachedTasks, task => Task.process(task, 2));
+      taskArea.setState( {suggestionsInTask: suggestions} );
+      taskList.setState({tasksInList : _.values(cachedTasks)});
+    });
 
-    fetchMessages(window.globalStateTaskId)
-      .then(function(messages) {
-        taskArea.setState( {messagesInTask: messages} );
-      })
-      .then(function(){
-        var elem = document.getElementsByClassName('chat-display');
-        elem.scrollTop = elem.scrollHeight;
-      })
-
-    fetchSuggestions(window.globalStateTaskId)
-      .then(function(suggestions) {
-        taskArea.setState( {suggestionsInTask: suggestions} );
-      })  
+    if(window.globalStateTaskId) {
+      fetchMessages(window.globalStateTaskId)
+        .then(function(messages) {
+          taskArea.setState( {messagesInTask: messages} );
+        })
+        .then(function(){
+          var elem = document.getElementsByClassName('chat-display');
+          elem.scrollTop = elem.scrollHeight;
+        })
+      }
   }, interval)
 }
 
@@ -84,5 +99,7 @@ function setUserVoteStatus(suggestion, userID, userHasVoted) {
       suggestion.user_vote_status = userHasVoted ? 'voted_on_other' : 'no_vote';
     }   
 }
+
+
 
 
