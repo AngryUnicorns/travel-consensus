757381730ace6aa10c326a3ca3fa5f745e3d8dc8
Clay Branch <clay.branch@gmail.com>
2016-02-26 12:22:56 -0600

    clicking downvote now deletes the vot

--- client/components/VoteIcon.jsx
+++ client/components/VoteIcon.jsx
@@ -1,12 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var PostHelper = require('../requests/post.js');
+var DeleteHelper = require('../requests/delete.js');
 var User = require('../models/users');
 var _ = require('underscore');
+var getVoteForUser = function(votes) {
+  var userID = User.getID();
+  return votes.filter(function(vote){
+    return vote.user_id === userID;
+  })[0].id;
+}
 var VoteIcon = React.createClass({
   voteHandler: function() {
     suggestionId = this.props.suggestion.id;
-    PostHelper.upVoteSuggestion(suggestionId, User.getID());
+    if(this.props.suggestion.user_vote_status === 'no_vote') {
+      PostHelper.upVoteSuggestion(suggestionId, User.getID());
+    } else {
+      var voteID = getVoteForUser(this.props.suggestion.votes);
+      DeleteHelper.downVoteSuggestion(suggestionId, voteID);
+    }  
   },
   arrowHelper: function() {
     if(this.props.suggestion.user_vote_status === 'voted_on_this') return 'fa fa-arrow-down';
